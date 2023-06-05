8a9b129dcf7c0a76d81e3cd50241d67880119c1b
Hugo <hugogreg@live.fr>
2023-06-05 17:58:36 +0200

    structure

--- assets/js/main.js
+++ assets/js/main.js
@@ -260,3 +260,56 @@
   new PureCounter();
 
 })()
+
+document.getElementById("form").addEventListener("submit", function (event) {
+  // Prevent the default form submission
+  event.preventDefault();
+
+  // Disable the submit button
+  var submitButton = document.getElementById("submitButton");
+  submitButton.disabled = true;
+
+  // Enable the submit button after 3 seconds
+  setTimeout(function () {
+    submitButton.disabled = false;
+  }, 3000);
+
+  // Submit the form
+  this.submit();
+});
+
+  /**
+   * Clients Slider
+   */
+  new Swiper('.clients-slider', {
+    speed: 400,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 2,
+        spaceBetween: 40
+      },
+      480: {
+        slidesPerView: 3,
+        spaceBetween: 60
+      },
+      640: {
+        slidesPerView: 4,
+        spaceBetween: 80
+      },
+      992: {
+        slidesPerView: 6,
+        spaceBetween: 120
+      }
+    }
+  });
